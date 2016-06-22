5f73b4bc7915111a21edfa1774a52cb040587921
kushanJ <kushan.joshi@gmail.com>
2016-06-22 13:20:25 -0400

    fancy output with comments

--- COMP125-S2016-MidTerm-300873962/Scripts/app.js
+++ COMP125-S2016-MidTerm-300873962/Scripts/app.js
@@ -4,18 +4,18 @@
 
 document.getElementById("IntroParagraph").innerHTML = "Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals. Greece has 1,400 islands, though only 230 of them are inhabited. And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too."
 
-console.log("1")
+
 document.getElementById("Folegandros").innerHTML  = "Folegandros is almost a Greek cliché, full of beautiful whitewashed houses with bright blue doors lining cobblestoned streets on steep cliffs. Rugged and remote, without chain hotels or package tours — not even a bank or ATM — and accessible only by ferry, the volcanic island boasts solitude in spades, secluded beaches, and incredible sunsets."
 
-console.log("2")
+
 document.getElementById("Alonissos").innerHTML = "Off the mainland, this island, whose surrounding waters are a designated marine park full of seals and dolphins, is a nature lover's dream. The spectacular spot's crystal clear waters and beautiful beaches are perfect for diving, and its lush flora and dense forests ideal for hikes. Since the rich landscape is chock full of indigenous herbs, it's also known for its traditional medicine, and home of the International Academy of Homeopathy."
 
-console.log("3")
+
 document.getElementById("Spetses").innerHTML = "This bohemian paradise is more Côte d'Azur than Grecian island. Spetses was hot in the '60s, when it hosted Marilyn Monroe and Elizabeth Taylor, and remains a swanky summer retreat for Athens' glitterati to this day, full of fancy hotels, mansions and yachts, and stylish boutiques. Aside from a handful of locally-owned cabs, the four-mile-long island has banned cars in town, giving it a refreshingly 19th-century vibe, with horse-drawn carriages being a common sight."
 
-console.log("4")
+
 document.getElementById("Amorgos").innerHTML = "Shipwrecks, sea caves and beautiful clear waters make Amorgos popular with snorkelers, but the mountainous island is the perfect compromise between unwinding and adventure, boasting beautiful beaches but also hiking, scuba diving and rock-climbing. One of the island's main attractions is the 11th-century cliffside monastery of Panayia Hozoviótissa, which is precariously carved into a cliff."
-console.log("5")
+
 document.getElementById("Syros").innerHTML = "A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners. Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways."
 
 
@@ -45,11 +45,17 @@ var contactNumber = document.getElementById("contactNumber").value
 var email =  document.getElementById("email").value
 
 var yourMessage =  document.getElementById("yourMessage").value
+console.log("==========================");
 console.log("firstName = " + firstName);
+console.log("==========================");
 console.log("lastName = " +  lastName);
-console.log("contactNumber = " + contactNumber)
-console.log("email = " + email)
-console.log(" yourMessage = " +  yourMessage)
+console.log("==========================");
+console.log("contactNumber = " + contactNumber);
+console.log("==========================");
+console.log("email = " + email);
+console.log("==========================");
+console.log(" yourMessage = " +  yourMessage);
+console.log("==========================");
 
 }
 
